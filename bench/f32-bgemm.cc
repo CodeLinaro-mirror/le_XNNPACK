2c65c9bac169186a2def9e26ea08d033d7328f65
Frank Barchard <fbarchard@google.com>
2023-04-12 16:43:22 -0700

    PPMM 4x8 with packx in 1 pass
    PiperOrigin-RevId: 523839810

--- bench/f32-bgemm.cc
+++ bench/f32-bgemm.cc
@@ -28,6 +28,8 @@
 #include <xnnpack/microparams-init.h>
 #include <xnnpack/pack.h>
 #include <xnnpack/packw.h>
+#include <xnnpack/packx.h>
+#include <xnnpack/ppmm.h>
 
 
 static void f32_gemm(benchmark::State& state,
@@ -80,6 +82,76 @@ static void f32_gemm(benchmark::State& state,
         gemm(
           mb, dim_n, dim_k * sizeof(float),
           a.data() + (i * dim_m + m) * dim_k, dim_k * sizeof(float),
+          w.data() + i * stride_n * (stride_k + 1),
+          c.data() + (i * dim_m + m) * dim_n, dim_n * sizeof(float), nr * sizeof(float),
+          &params);
+      }
+    }
+  }
+
+  const uint64_t cpu_frequency = benchmark::utils::GetCurrentCpuFrequency();
+  if (cpu_frequency != 0) {
+    state.counters["cpufreq"] = cpu_frequency;
+  }
+
+  state.counters["FLOPS"] = benchmark::Counter(
+    uint64_t(state.iterations()) * 2 * batch * dim_m * dim_n * dim_k, benchmark::Counter::kIsRate);
+}
+
+static void f32_ppmm1p(benchmark::State& state,
+  xnn_x32_packw_gemm_goi_ukernel_fn packw,
+  xnn_x32_packx_ukernel_fn packx,
+  xnn_f32_ppmm_minmax_ukernel_fn ppmm,
+  xnn_init_f32_minmax_params_fn init_params,
+  size_t mr, size_t nr, size_t kr, size_t sr,
+  benchmark::utils::IsaCheckFunction isa_check = nullptr)
+{
+  if (isa_check != nullptr && !isa_check(state)) {
+    return;
+  }
+
+  const size_t batch = state.range(0);
+  const size_t dim_m = state.range(1);
+  const size_t dim_n = state.range(2);
+  const size_t dim_k = state.range(3);
+
+  const size_t stride_n = benchmark::utils::RoundUp(dim_n, nr);
+  const size_t stride_k = benchmark::utils::RoundUp(dim_k, kr * sr);
+
+  std::random_device random_device;
+  auto rng = std::mt19937(random_device());
+  auto f32rng = std::bind(std::uniform_real_distribution<float>(), std::ref(rng));
+
+  std::vector<float> a(batch * dim_m * dim_k + XNN_EXTRA_BYTES / sizeof(float));
+  std::generate(a.begin(), a.end(), std::ref(f32rng));
+  std::vector<float> b(batch * dim_n * dim_k);
+  std::generate(b.begin(), b.end(), std::ref(f32rng));
+  std::vector<float> c(batch * dim_m * dim_n);
+  std::fill(c.begin(), c.end(), std::nanf(""));
+
+  std::vector<uint32_t, AlignedAllocator<uint32_t, 64>> t(mr * dim_k);
+
+  const size_t w_elements = stride_n * stride_k + stride_n;
+  std::vector<float, AlignedAllocator<float, 64>> w(batch * w_elements);
+  std::fill(w.begin(), w.end(), 0.0f);
+
+  xnn_f32_minmax_params params;
+  init_params(&params,
+    -std::numeric_limits<float>::infinity(), +std::numeric_limits<float>::infinity());
+
+  for (auto _ : state) {
+    packw(batch, dim_n, dim_k, nr, kr, sr,
+      reinterpret_cast<const uint32_t*>(b.data()), /*bias=*/nullptr,
+      reinterpret_cast<uint32_t*>(w.data()),
+      /*extra_bytes=*/0, nullptr);
+
+    for (size_t i = 0; i < batch; i++) {
+      for (size_t m = 0; m < dim_m; m += mr) {
+        const size_t mb = min(dim_m - m, mr);
+        packx(mb, dim_k, reinterpret_cast<const uint32_t*>(a.data() + m * dim_k), dim_k, t.data());
+        ppmm(
+          mb, dim_n, dim_k * sizeof(float),
+          reinterpret_cast<const float*>(t.data()),
           w.data() + i * stride_n * (stride_k + 1),
           c.data() + (i * dim_m + m) * dim_n, dim_n * sizeof(float), nr * sizeof(float),
           &params);
@@ -299,6 +371,14 @@ static void ruy_st(benchmark::State& state, const char* net)
       xnn_init_f32_minmax_scalar_params,
       /*mr=*/6, /*nr=*/8, /*kr=*/1, /*sr=*/1);
   }
+  static void f32_ppmm_4x8_unipass__neonfma(benchmark::State& state, const char* net) {
+    f32_ppmm1p(state,
+      xnn_x32_packw_gemm_goi_ukernel_x8__neon_ld4lane_prfm_x4,
+      xnn_x32_packx_ukernel_4x__neon_st4,
+      xnn_f32_ppmm_minmax_ukernel_4x8__neonfma,
+      xnn_init_f32_minmax_scalar_params,
+      /*mr=*/4, /*nr=*/8, /*kr=*/1, /*sr=*/1);
+  }
 
   BENCHMARK_BGEMM(f32_gemm_4x8__asm_aarch64_neonfma_cortex_a53)
   BENCHMARK_BGEMM(f32_gemm_4x8__asm_aarch64_neonfma_prfm_cortex_a53)
@@ -318,6 +398,7 @@ static void ruy_st(benchmark::State& state, const char* net)
   BENCHMARK_BGEMM(f32_gemm_6x8__asm_aarch64_neonfma_prfm_cortex_a75)
   BENCHMARK_BGEMM(f32_gemm_6x8__asm_aarch64_neonfma_ld64)
   BENCHMARK_BGEMM(f32_gemm_6x8__asm_aarch64_neonfma_ld128)
+  BENCHMARK_BGEMM(f32_ppmm_4x8_unipass__neonfma)
 #endif  // XNN_ARCH_ARM64 && XNN_ENABLE_ASSEMBLY
 
 #if XNN_ARCH_ARM && XNN_ENABLE_ASSEMBLY
