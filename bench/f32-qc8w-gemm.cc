bb502d7e28d36801282f2aa9f554e4c9664d37a1
Milen Ferev <milenferev@google.com>
2023-05-01 09:27:27 -0700

    F32-QC8-GEMM 1x8, 4x8, 6x8 ld128 Neon microkernels
    PiperOrigin-RevId: 528490976

--- bench/f32-qc8w-gemm.cc
+++ bench/f32-qc8w-gemm.cc
@@ -135,12 +135,37 @@ static void GEMMBenchmark(benchmark::State& state,
       xnn_init_f32_minmax_scalar_params,
       /*mr=*/4, /*nr=*/2, /*kr=*/1, /*sr=*/1);
   }
-
   BENCHMARK_GEMM(f32_qc8w_gemm_1x8__asm_aarch64_neonfma_ld64)
   BENCHMARK_GEMM(f32_qc8w_gemm_4x8__asm_aarch64_neonfma_ld64)
   BENCHMARK_GEMM(f32_qc8w_gemm_6x8__asm_aarch64_neonfma_ld64)
   BENCHMARK_GEMM(f32_qc8w_gemm_4x2__asm_aarch64_neonfma_ld64)
 #endif  // XNN_ARCH_ARM64 && XNN_ENABLE_ASSEMBLY
+
+
+#if XNN_ARCH_ARM64
+  static void f32_qc8w_gemm_minmax_ukernel_1x8__aarch64_neonfma_lane_ld128(benchmark::State& state, const char* net) {
+    GEMMBenchmark(state,
+      xnn_f32_qc8w_gemm_minmax_ukernel_1x8__aarch64_neonfma_lane_ld128,
+      xnn_init_f32_minmax_scalar_params,
+      /*mr=*/1, /*nr=*/8, /*kr=*/1, /*sr=*/1);
+  }
+  static void f32_qc8w_gemm_minmax_ukernel_4x8__aarch64_neonfma_lane_ld128(benchmark::State& state, const char* net) {
+    GEMMBenchmark(state,
+      xnn_f32_qc8w_gemm_minmax_ukernel_4x8__aarch64_neonfma_lane_ld128,
+      xnn_init_f32_minmax_scalar_params,
+      /*mr=*/4, /*nr=*/8, /*kr=*/1, /*sr=*/1);
+  }
+  static void f32_qc8w_gemm_minmax_ukernel_6x8__aarch64_neonfma_lane_ld128(benchmark::State& state, const char* net) {
+    GEMMBenchmark(state,
+      xnn_f32_qc8w_gemm_minmax_ukernel_6x8__aarch64_neonfma_lane_ld128,
+      xnn_init_f32_minmax_scalar_params,
+      /*mr=*/6, /*nr=*/8, /*kr=*/1, /*sr=*/1);
+  }
+
+  BENCHMARK_GEMM(f32_qc8w_gemm_minmax_ukernel_1x8__aarch64_neonfma_lane_ld128)
+  BENCHMARK_GEMM(f32_qc8w_gemm_minmax_ukernel_4x8__aarch64_neonfma_lane_ld128)
+  BENCHMARK_GEMM(f32_qc8w_gemm_minmax_ukernel_6x8__aarch64_neonfma_lane_ld128)
+#endif  // XNN_ARCH_ARM64
 
 static void f32_qc8w_gemm_1x4__scalar(benchmark::State& state, const char* net) {
   GEMMBenchmark(state,
--- scripts/generate-f32-qc8w-gemm.sh
+++ scripts/generate-f32-qc8w-gemm.sh
@@ -40,6 +40,12 @@ tools/xngen src/f32-gemm/4x8-aarch64-neonfma-ld64.S.in -D INC=0 -D DATATYPE=QC8 
 tools/xngen src/f32-gemm/6x8-aarch64-neonfma-ld64.S.in -D INC=0 -D DATATYPE=QC8 -o src/f32-qc8w-gemm/gen/f32-qc8w-gemm-6x8-minmax-asm-aarch64-neonfma-ld64.S &
 tools/xngen src/f32-gemm/4x2-aarch64-neonfma-ld64.S.in -D INC=0 -D DATATYPE=QC8 -o src/f32-qc8w-gemm/gen/f32-qc8w-gemm-4x2-minmax-asm-aarch64-neonfma-ld64.S &
 
+################################### ARM NEON ##################################
+### LD128 micro-kernels
+tools/xngen src/f32-gemm/neon-ld128.c.in -D MR=1 -D NR=8 -D INC=0 -D FMA=1 -D DUP=0 -D DATATYPE=QC8 -o src/f32-qc8w-gemm/gen/f32-qc8w-gemm-1x8-minmax-aarch64-neonfma-ld128.c &
+tools/xngen src/f32-gemm/neon-ld128.c.in -D MR=4 -D NR=8 -D INC=0 -D FMA=1 -D DUP=0 -D DATATYPE=QC8 -o src/f32-qc8w-gemm/gen/f32-qc8w-gemm-4x8-minmax-aarch64-neonfma-ld128.c &
+tools/xngen src/f32-gemm/neon-ld128.c.in -D MR=6 -D NR=8 -D INC=0 -D FMA=1 -D DUP=0 -D DATATYPE=QC8 -o src/f32-qc8w-gemm/gen/f32-qc8w-gemm-6x8-minmax-aarch64-neonfma-ld128.c &
+
 ################################## Unit tests #################################
 tools/generate-gemm-test.py --spec test/f32-qc8w-gemm.yaml        --output test/f32-qc8w-gemm.cc        &
 tools/generate-gemm-test.py --spec test/f32-qc8w-gemm-relu.yaml   --output test/f32-qc8w-gemm-relu.cc   &
