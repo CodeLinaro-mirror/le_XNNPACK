761f52171ec134a9aed38146c39f5817566bf9be
Valeriy Avanesov <avanesov@google.com>
2023-06-14 02:03:15 -0700

    Unify first function access for JIT code
    This adds a function `void* xnn_first_function_ptr(xnn_code_buffer*)`,
    providing a homogeneous interface for invoking the JIT kernels be it
    native or WASM JIT.
    
    PiperOrigin-RevId: 540204958

--- src/xnnpack/memory.h
+++ src/xnnpack/memory.h
@@ -37,6 +37,7 @@ struct xnn_code_buffer {
 #if XNN_PLATFORM_WEB
   // The index of the first function added by loading a WASM module. The field
   // is set by xnn_finalize_code_memory.
+  // Use xnn_first_function_ptr function to get the value.
   int first_function_index;
 #endif
 };
@@ -50,6 +51,10 @@ enum xnn_status xnn_release_code_memory(struct xnn_code_buffer* buffer);
 #if XNN_PLATFORM_JIT
 // Finalize buffer, users won't need to call this directly, called by Assembler.
 enum xnn_status xnn_finalize_code_memory(struct xnn_code_buffer* buffer);
+// Returns a pointer to the first function in the buffer. Should be called only
+// after xnn_finalize_code_memory had been called on the buffer, may return NULL
+// otherwise.
+void* xnn_first_function_ptr(const struct xnn_code_buffer* buffer);
 #endif
 
 // Buffer to hold repacked weights.
