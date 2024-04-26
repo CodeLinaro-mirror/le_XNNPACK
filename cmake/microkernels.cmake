c9035698525f80ca24fbb853885b717576fc1d4e
Alan Kelly <alankelly@google.com>
2024-04-26 02:19:22 -0700

    Add F16F32ACC AVX512SKX rdsum accumulating microkernels
    PiperOrigin-RevId: 628338857

--- cmake/microkernels.cmake
+++ cmake/microkernels.cmake
@@ -1324,6 +1324,10 @@ SET(ALL_AVX512F_MICROKERNEL_SRCS
   src/f32-raddstoreexpminusmax/gen/f32-raddstoreexpminusmax-avx512f-rr1-p5-scalef-u192-acc3.c
   src/f32-raddstoreexpminusmax/gen/f32-raddstoreexpminusmax-avx512f-rr1-p5-scalef-u192-acc6.c
   src/f32-raddstoreexpminusmax/gen/f32-raddstoreexpminusmax-avx512f-rr1-p5-scalef-u192.c
+  src/f32-rdsum/gen/f32-rdsum-7p7x-minmax-avx512f-c16.c
+  src/f32-rdsum/gen/f32-rdsum-7p7x-minmax-avx512f-c32.c
+  src/f32-rdsum/gen/f32-rdsum-7p7x-minmax-avx512f-c64.c
+  src/f32-rdsum/gen/f32-rdsum-7p7x-minmax-avx512f-c128.c
   src/f32-rminmax/gen/f32-rmax-avx512f-u16.c
   src/f32-rminmax/gen/f32-rmax-avx512f-u32-acc2.c
   src/f32-rminmax/gen/f32-rmax-avx512f-u48-acc3.c
@@ -1566,6 +1570,10 @@ SET(ALL_AVX512FP16_MICROKERNEL_SRCS
 SET(ALL_AVX512SKX_MICROKERNEL_SRCS
   src/f16-f32-vcvt/gen/f16-f32-vcvt-avx512skx-u16.c
   src/f16-f32-vcvt/gen/f16-f32-vcvt-avx512skx-u32.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-avx512skx-c16.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-avx512skx-c32.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-avx512skx-c64.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-avx512skx-c128.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-avx512skx-u16.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-avx512skx-u32-acc2.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-avx512skx-u48-acc3.c
@@ -4608,6 +4616,9 @@ SET(ALL_NEONFP16ARITH_MICROKERNEL_SRCS
   src/f16-dwconv/gen/f16-dwconv-25p16c-minmax-neonfp16arith.c
   src/f16-dwconv/gen/f16-dwconv-25p32c-minmax-neonfp16arith-acc2.c
   src/f16-dwconv/gen/f16-dwconv-25p32c-minmax-neonfp16arith.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-minmax-neonfp16arith-c16.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-minmax-neonfp16arith-c32.c
+  src/f16-f32acc-rdsum/gen/f16-f32acc-rdsum-7p7x-minmax-neonfp16arith-c64.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-neonfp16arith-u4.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-neonfp16arith-u8.c
   src/f16-f32acc-rsum/gen/f16-f32acc-rsum-neonfp16arith-u16-acc2.c
