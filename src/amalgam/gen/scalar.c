CodeLinaro-mirror/le_external_github.com_google_XNNPACK

Extend the `convert` operator for the new `qp8` packed per-row dynamic quantization.
PiperOrigin-RevId: 637591037

src/amalgam/gen/scalar.c
@@ -5,6 +5,7 @@
 
 #include <fp16/fp16.h>
 #include <assert.h>
+#include <float.h>
 #include <fxdiv.h>
 #include <math.h>
 #include <stddef.h>
@@ -25,6 +26,7 @@
 #include <xnnpack/math.h>
 #include <xnnpack/maxpool.h>
 #include <xnnpack/microparams.h>
+#include <xnnpack/packq.h>
 #include <xnnpack/packw.h>
 #include <xnnpack/pad.h>
 #include <xnnpack/pavgpool.h>
@@ -28398,6 +28400,118 @@ void xnn_x8_lut_ukernel__scalar_u4(
       *output++ = (uint8_t) vt;
       batch -= sizeof(uint8_t);
     } while (batch != 0);
+  }
+}
+
+void xnn_x8_packq_f32qp8_ukernel__scalar_u1(size_t m, size_t k, size_t mr,
+                                            size_t kr, size_t sr,
+                                            size_t m_idx_start,
+                                            const float* XNN_RESTRICT lhs,
+                                            size_t lhs_stride,
+                                            void* XNN_RESTRICT lhs_packed) {
+  assert((kr % sr) == 0);
+
+  // Assuming the same sizeof() for kai_num_bytes_per_offset and
+  // kai_num_bytes_per_multiplier
+  static const size_t num_bytes_per_multiplier = sizeof(float);
+  static const size_t num_bytes_per_offset = sizeof(int32_t);
+  assert(num_bytes_per_offset == num_bytes_per_multiplier);
+
+  if (m == 0) {
+    return;
+  }
+
+  const size_t num_rows = m;
+
+  const float* src_ptr = lhs;
+
+  const size_t dst_stride = kai_lhs_packed_stride(k, mr, kr, sr);
+  const size_t k_internal = kai_k_roundedup(k, kr, sr);
+  const int32_t k_block_len = (int32_t)(kr / sr);
+
+  for (size_t row_idx = 0; row_idx < num_rows; ++row_idx) {
+    float max0 = 0.0f;
+    float min0 = 0.0f;
+
+    // Find min/max for each channel
+    int32_t k_idx = 0;
+    for (; k_idx < (int32_t)k; ++k_idx) {
+      const float src0_0 = *(src_ptr + (size_t)k_idx);
+      max0 = math_max_f32(src0_0, max0);
+      min0 = math_min_f32(src0_0, min0);
+    }
+
+    // Maximum/minimum int8 values
+    const float qmin = (float)INT8_MIN;
+    const float qmax = (float)INT8_MAX;
+
+    const float scale0 = min0 == max0 ? 1.F : (qmax - qmin) / (max0 - min0);
+
+    // Reciprocal to quantize
+    const float recip_scale0 = scale0 ? 1.0F / scale0 : 0.0F;
+
+    const float descaled_min0 = min0 * scale0;
+    const float descaled_max0 = max0 * scale0;
+
+    const float zero_point_from_min_error0 = qmin + descaled_min0;
+    const float zero_point_from_max_error0 = qmax + descaled_max0;
+
+    float zero_point0 =
+        zero_point_from_min_error0 + zero_point_from_max_error0 > 0
+            ? qmin - descaled_min0
+            : qmax - descaled_max0;
+
+    zero_point0 = math_max_f32(zero_point0, qmin);
+    zero_point0 = math_min_f32(zero_point0, qmax);
+
+    // Round to nearest integer
+    const int32_t nudged_zero_point0 = (int32_t)rintf(zero_point0);
+
+    const size_t dst_x = ((row_idx + m_idx_start) % mr);
+
+    uint8_t* dst_ptr =
+        (uint8_t*)lhs_packed + dst_x * k_block_len * sizeof(int8_t);
+
+    // Quantize the channels
+    k_idx = 0;
+    for (; k_idx < (int32_t)k_internal; k_idx += k_block_len) {
+      for (size_t k_block_idx = 0; k_block_idx < (size_t)k_block_len;
+           ++k_block_idx) {
+        // Clamp at the last valid k-index
+        const size_t k_idx_start = min((size_t)k_idx + k_block_idx, k);
+
+        const float src0_0 = *(src_ptr + k_idx_start);
+
+        // Scale the values
+        int32_t v0_s32 = (int32_t)(roundf(src0_0 * scale0));
+
+        v0_s32 = v0_s32 + nudged_zero_point0;
+        v0_s32 = math_max_s32(v0_s32, INT8_MIN);
+        v0_s32 = math_min_s32(v0_s32, INT8_MAX);
+        *((int8_t*)(dst_ptr)) = (int8_t)v0_s32;
+        dst_ptr += sizeof(int8_t);
+      }
+      dst_ptr += (mr - 1) * k_block_len * sizeof(int8_t);
+    }
+
+    dst_ptr = (uint8_t*)lhs_packed + mr * (k_internal * sizeof(int8_t));
+
+    dst_ptr += dst_x * num_bytes_per_offset;
+
+    // LHS offset at the beginning of the row
+    *((int32_t*)(dst_ptr)) = -nudged_zero_point0;
+
+    dst_ptr += mr * num_bytes_per_multiplier;
+
+    // Store the scale quantization params
+    *((float*)(dst_ptr)) = recip_scale0;
+
+    src_ptr += (lhs_stride / sizeof(float));
+
+    // Move to the next row if we have interleaved all Mr rows
+    if ((((row_idx + 1) + m_idx_start) % mr) == 0) {
+      lhs_packed = (void*)((uint8_t*)lhs_packed + dst_stride);
+    }
   }
 }
 

CodeLinaro-mirror/le_XNNPACK

src/amalgam/gen/scalar.c
@@ -5,6 +5,7 @@
 
 #include <fp16/fp16.h>
 #include <assert.h>
+#include <float.h>
 #include <fxdiv.h>
 #include <math.h>
 #include <simd/f32-scalar.h>
@@ -26,6 +27,7 @@
 #include <xnnpack/math.h>
 #include <xnnpack/maxpool.h>
 #include <xnnpack/microparams.h>
+#include <xnnpack/packq.h>
 #include <xnnpack/packw.h>
 #include <xnnpack/pad.h>
 #include <xnnpack/pavgpool.h>
@@ -28220,6 +28222,140 @@ void xnn_x8_lut_ukernel__scalar_u4(
       *output++ = (uint8_t) vt;
       batch -= sizeof(uint8_t);
     } while (batch != 0);
+  }
+}
+
+inline static size_t k_roundedup(size_t k, size_t kr, size_t sr) {
+  // Since we pack a float and int32 value at the end of the row,
+  // we must make sure that k is a multiple of 4 for memory alignment.
+  size_t kr_sr_roundedup4 = round_up(kr * sr, 4);
+  return round_up(k, kr_sr_roundedup4);
+}
+
+inline static size_t lhs_packed_stride(size_t k, size_t mr, size_t kr,
+                                       size_t sr) {
+  const size_t k_internal = k_roundedup(k, kr, sr);
+
+  assert((k_internal % 2) == 0);
+
+  // Assuming the same sizeof() for kai_num_bytes_per_offset and
+  // kai_num_bytes_per_multiplier
+  static const size_t num_bytes_per_multiplier = sizeof(float);
+  static const size_t num_bytes_per_offset = sizeof(int32_t);
+
+  return mr * (k_internal * sizeof(int8_t) + num_bytes_per_multiplier +
+               num_bytes_per_offset);
+}
+
+void xnn_x8_packq_f32qp8_ukernel__scalar_u1(size_t m, size_t k, size_t mr,
+                                            size_t kr, size_t sr,
+                                            size_t m_idx_start,
+                                            const float* XNN_RESTRICT lhs,
+                                            size_t lhs_stride,
+                                            void* XNN_RESTRICT lhs_packed) {
+  assert((kr % sr) == 0);
+
+  // Assuming the same sizeof() for kai_num_bytes_per_offset and
+  // kai_num_bytes_per_multiplier
+  static const size_t num_bytes_per_multiplier = sizeof(float);
+  static const size_t num_bytes_per_offset = sizeof(int32_t);
+  assert(num_bytes_per_offset == num_bytes_per_multiplier);
+
+  if (m == 0) {
+    return;
+  }
+
+  const size_t num_rows = m;
+
+  const float* src_ptr = lhs;
+
+  const size_t dst_stride = lhs_packed_stride(k, mr, kr, sr);
+  const size_t k_internal = k_roundedup(k, kr, sr);
+  const int32_t k_block_len = (int32_t)(kr / sr);
+
+  for (size_t row_idx = 0; row_idx < num_rows; ++row_idx) {
+    float max0 = 0.0f;
+    float min0 = 0.0f;
+
+    // Find min/max for each channel
+    int32_t k_idx = 0;
+    for (; k_idx < (int32_t)k; ++k_idx) {
+      const float src0_0 = *(src_ptr + (size_t)k_idx);
+      max0 = math_max_f32(src0_0, max0);
+      min0 = math_min_f32(src0_0, min0);
+    }
+
+    // Maximum/minimum int8 values
+    const float qmin = (float)INT8_MIN;
+    const float qmax = (float)INT8_MAX;
+
+    const float scale0 = min0 == max0 ? 1.F : (qmax - qmin) / (max0 - min0);
+
+    // Reciprocal to quantize
+    const float recip_scale0 = scale0 ? 1.0F / scale0 : 0.0F;
+
+    const float descaled_min0 = min0 * scale0;
+    const float descaled_max0 = max0 * scale0;
+
+    const float zero_point_from_min_error0 = qmin + descaled_min0;
+    const float zero_point_from_max_error0 = qmax + descaled_max0;
+
+    float zero_point0 =
+        zero_point_from_min_error0 + zero_point_from_max_error0 > 0
+            ? qmin - descaled_min0
+            : qmax - descaled_max0;
+
+    zero_point0 = math_max_f32(zero_point0, qmin);
+    zero_point0 = math_min_f32(zero_point0, qmax);
+
+    // Round to nearest integer
+    const int32_t nudged_zero_point0 = (int32_t)rintf(zero_point0);
+
+    const size_t dst_x = ((row_idx + m_idx_start) % mr);
+
+    uint8_t* dst_ptr =
+        (uint8_t*)lhs_packed + dst_x * k_block_len * sizeof(int8_t);
+
+    // Quantize the channels
+    k_idx = 0;
+    for (; k_idx < (int32_t)k_internal; k_idx += k_block_len) {
+      for (size_t k_block_idx = 0; k_block_idx < (size_t)k_block_len;
+           ++k_block_idx) {
+        // Clamp at the last valid k-index
+        const size_t k_idx_start = min((size_t)k_idx + k_block_idx, k);
+
+        const float src0_0 = *(src_ptr + k_idx_start);
+
+        // Scale the values
+        int32_t v0_s32 = (int32_t)(roundf(src0_0 * scale0));
+
+        v0_s32 = v0_s32 + nudged_zero_point0;
+        v0_s32 = math_max_s32(v0_s32, INT8_MIN);
+        v0_s32 = math_min_s32(v0_s32, INT8_MAX);
+        *((int8_t*)(dst_ptr)) = (int8_t)v0_s32;
+        dst_ptr += sizeof(int8_t);
+      }
+      dst_ptr += (mr - 1) * k_block_len * sizeof(int8_t);
+    }
+
+    dst_ptr = (uint8_t*)lhs_packed + mr * (k_internal * sizeof(int8_t));
+
+    dst_ptr += dst_x * num_bytes_per_offset;
+
+    // LHS offset at the beginning of the row
+    *((int32_t*)(dst_ptr)) = -nudged_zero_point0;
+
+    dst_ptr += mr * num_bytes_per_multiplier;
+
+    // Store the scale quantization params
+    *((float*)(dst_ptr)) = recip_scale0;
+
+    src_ptr += (lhs_stride / sizeof(float));
+
+    // Move to the next row if we have interleaved all Mr rows
+    if ((((row_idx + 1) + m_idx_start) % mr) == 0) {
+      lhs_packed = (void*)((uint8_t*)lhs_packed + dst_stride);
+    }
   }
 }
 