07bd4c4c66ec510bca8923307cfd74875d804c45
Pedro Gonnet <gonnet@google.com>
2024-07-17 09:11:17 -0700

    Try using `ccache` for `msvc` builds.
    PiperOrigin-RevId: 653254809

--- scripts/build-windows-x64.cmd
+++ scripts/build-windows-x64.cmd
@@ -1,8 +1,12 @@
 mkdir build\windows
 mkdir build\windows\x64
 
+cmd vcvarsall.bat x64
+
 set CMAKE_ARGS=-DXNNPACK_LIBRARY_TYPE=static
-set CMAKE_ARGS=%CMAKE_ARGS% -G="Visual Studio 17 2022" -A=x64 -DCMAKE_CXX_FLAGS="/MP"
+set CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+@REM set CMAKE_ARGS=%CMAKE_ARGS% -G="Visual Studio 17 2022" -A=x64 -DCMAKE_CXX_FLAGS="/MP"
+set CMAKE_ARGS=%CMAKE_ARGS% -G="Ninja" -DCMAKE_CXX_FLAGS="/MP"
 
 rem Use-specified CMake arguments go last to allow overridding defaults
 set CMAKE_ARGS=%CMAKE_ARGS% %*
