CodeLinaro-mirror/le_external_github.com_google_XNNPACK

Add `xnn_float16_t` for use instead of `uint16_t`
This should improve type safety a lot (this CL fixes a bunch of latent bugs already), and allows C++ template code to parameterize on data types of microkernels more easily, because this enables template functions to work on float16 data without special case code.

PiperOrigin-RevId: 667840524

test/global-sum-pooling-operator-tester.h
@@ -20,10 +20,10 @@
 #include <vector>
 
 #include <gtest/gtest.h>
-#include <fp16/fp16.h>
 #include "xnnpack.h"
 #include "xnnpack/aligned-allocator.h"
 #include "xnnpack/common.h"
+#include "xnnpack/math.h"
 #include "replicable_random_device.h"
 
 class GlobalSumPoolingOperatorTester {
@@ -119,19 +119,19 @@ class GlobalSumPoolingOperatorTester {
     xnnpack::ReplicableRandomDevice rng;
     std::uniform_real_distribution<float> f32dist(1.0e-3f, 1.0f);
 
-    std::vector<uint16_t> input((batch_size() * width() - 1) * input_stride() + channels() + XNN_EXTRA_BYTES / sizeof(uint16_t));
-    std::vector<uint16_t> output(batch_size() * output_stride());
+    std::vector<xnn_float16_t> input((batch_size() * width() - 1) * input_stride() + channels() + XNN_EXTRA_BYTES / sizeof(xnn_float16_t));
+    std::vector<xnn_float16_t> output(batch_size() * output_stride());
     std::vector<float> output_ref(batch_size() * channels());
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return fp16_ieee_from_fp32_value(f32dist(rng)); });
-      std::fill(output.begin(), output.end(), UINT16_C(0x7E00) /* NaN */);
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
+      std::fill(output.begin(), output.end(), std::nanf(""));
 
       // Compute reference results, without clamping.
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t j = 0; j < channels(); j++) {
           float acc = 0.0f;
           for (size_t k = 0; k < width(); k++) {
-            acc += fp16_ieee_to_fp32_value(input[(i * width() + k) * input_stride() + j]);
+            acc += input[(i * width() + k) * input_stride() + j];
           }
           output_ref[i * channels() + j] = acc;
         }
@@ -141,8 +141,8 @@ class GlobalSumPoolingOperatorTester {
       const float accumulated_min = *std::min_element(output_ref.cbegin(), output_ref.cend());
       const float accumulated_max = *std::max_element(output_ref.cbegin(), output_ref.cend());
       const float accumulated_range = accumulated_max - accumulated_min;
-      const float scaled_min = fp16_ieee_to_fp32_value(fp16_ieee_from_fp32_value(accumulated_min + accumulated_range / 255.0f * float(qmin())));
-      const float scaled_max = fp16_ieee_to_fp32_value(fp16_ieee_from_fp32_value(accumulated_max - accumulated_range / 255.0f * float(255 - qmax())));
+      const float scaled_min = xnn_float16_t(accumulated_min + accumulated_range / 255.0f * float(qmin()));
+      const float scaled_max = xnn_float16_t(accumulated_max - accumulated_range / 255.0f * float(255 - qmax()));
       const float output_min = scaled_min == scaled_max ? -std::numeric_limits<float>::infinity() : scaled_min;
       const float output_max = scaled_min == scaled_max ? +std::numeric_limits<float>::infinity() : scaled_max;
 
@@ -192,9 +192,9 @@ class GlobalSumPoolingOperatorTester {
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t c = 0; c < channels(); c++) {
-          EXPECT_LE(fp16_ieee_to_fp32_value(output[i * output_stride() + c]), output_max);
-          EXPECT_GE(fp16_ieee_to_fp32_value(output[i * output_stride() + c]), output_min);
-          EXPECT_NEAR(fp16_ieee_to_fp32_value(output[i * output_stride() + c]), output_ref[i * channels() + c], std::max(1.0e-4f, std::abs(output_ref[i * channels() + c]) * 1.0e-2f))
+          EXPECT_LE(output[i * output_stride() + c], output_max);
+          EXPECT_GE(output[i * output_stride() + c], output_min);
+          EXPECT_NEAR(output[i * output_stride() + c], output_ref[i * channels() + c], std::max(1.0e-4f, std::abs(output_ref[i * channels() + c]) * 1.0e-2f))
             << "at batch index " << i << " / " << batch_size()
             << ", channel " << c << " / " << channels();
         }

test/rsum-microkernel-tester.h
@@ -138,27 +138,27 @@ class RSumMicrokernelTester {
     xnnpack::ReplicableRandomDevice rng;
     std::uniform_real_distribution<float> f32dist(0.01f, 1.0f);
 
-    std::vector<uint16_t> input(batch_size() + XNN_EXTRA_BYTES / sizeof(uint16_t));
+    std::vector<xnn_float16_t> input(batch_size() + XNN_EXTRA_BYTES / sizeof(xnn_float16_t));
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return fp16_ieee_from_fp32_value(f32dist(rng)); });
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
 
       // Compute reference results.
       float output_ref = 0.0f;
       for (size_t i = 0; i < batch_size(); i++) {
-        output_ref += fp16_ieee_to_fp32_value(input[i]);
+        output_ref += input[i];
       }
       output_ref *= scale();
 
       // Prepare parameters.
       xnn_f16_scale_params params;
-      init_params(&params, fp16_ieee_from_fp32_value(scale()));
+      init_params(&params, scale());
 
       // Call optimized micro-kernel.
-      uint16_t output = UINT16_C(0x7E00);  /* NaN */
-      rsum(batch_size() * sizeof(uint16_t), input.data(), &output, &params);
+      xnn_float16_t output = std::nanf("");  /* NaN */
+      rsum(batch_size() * sizeof(xnn_float16_t), input.data(), &output, &params);
 
       // Verify results.
-      EXPECT_NEAR(fp16_ieee_to_fp32_value(output), output_ref, std::abs(output_ref) * 4.0e-3f)
+      EXPECT_NEAR(output, output_ref, std::abs(output_ref) * 4.0e-3f)
         << "with batch " << batch_size() << ", scale " << scale();
     }
   }
@@ -167,14 +167,14 @@ class RSumMicrokernelTester {
     xnnpack::ReplicableRandomDevice rng;
     std::uniform_real_distribution<float> f32dist(0.01f, 1.0f);
 
-    std::vector<uint16_t> input(batch_size() + XNN_EXTRA_BYTES / sizeof(uint16_t));
+    std::vector<xnn_float16_t> input(batch_size() + XNN_EXTRA_BYTES / sizeof(xnn_float16_t));
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return fp16_ieee_from_fp32_value(f32dist(rng)); });
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
 
       // Compute reference results.
       float output_ref = 0.0f;
       for (size_t i = 0; i < batch_size(); i++) {
-        output_ref += fp16_ieee_to_fp32_value(input[i]);
+        output_ref += input[i];
       }
       output_ref *= scale();
 
@@ -184,7 +184,7 @@ class RSumMicrokernelTester {
 
       // Call optimized micro-kernel.
       float output = 0.f;
-      rsum(batch_size() * sizeof(uint16_t), input.data(), &output, &params);
+      rsum(batch_size() * sizeof(xnn_float16_t), input.data(), &output, &params);
 
       // Verify results.
       EXPECT_NEAR(output, output_ref, std::abs(output_ref) * 1.0e-5f)

CodeLinaro-mirror/le_XNNPACK

test/global-sum-pooling-operator-tester.h
@@ -123,15 +123,15 @@ class GlobalSumPoolingOperatorTester {
     std::vector<xnn_float16> output(batch_size() * output_stride());
     std::vector<float> output_ref(batch_size() * channels());
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return xnn_float16_from_float(f32dist(rng)); });
-      std::fill(output.begin(), output.end(), UINT16_C(0x7E00) /* NaN */);
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
+      std::fill(output.begin(), output.end(), std::nanf(""));
 
       // Compute reference results, without clamping.
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t j = 0; j < channels(); j++) {
           float acc = 0.0f;
           for (size_t k = 0; k < width(); k++) {
-            acc += xnn_float16_to_float(input[(i * width() + k) * input_stride() + j]);
+            acc += input[(i * width() + k) * input_stride() + j];
           }
           output_ref[i * channels() + j] = acc;
         }
@@ -141,8 +141,8 @@ class GlobalSumPoolingOperatorTester {
       const float accumulated_min = *std::min_element(output_ref.cbegin(), output_ref.cend());
       const float accumulated_max = *std::max_element(output_ref.cbegin(), output_ref.cend());
       const float accumulated_range = accumulated_max - accumulated_min;
-      const float scaled_min = xnn_float16_to_float(xnn_float16_from_float(accumulated_min + accumulated_range / 255.0f * float(qmin())));
-      const float scaled_max = xnn_float16_to_float(xnn_float16_from_float(accumulated_max - accumulated_range / 255.0f * float(255 - qmax())));
+      const float scaled_min = xnn_float16(accumulated_min + accumulated_range / 255.0f * float(qmin()));
+      const float scaled_max = xnn_float16(accumulated_max - accumulated_range / 255.0f * float(255 - qmax()));
       const float output_min = scaled_min == scaled_max ? -std::numeric_limits<float>::infinity() : scaled_min;
       const float output_max = scaled_min == scaled_max ? +std::numeric_limits<float>::infinity() : scaled_max;
 
@@ -192,9 +192,9 @@ class GlobalSumPoolingOperatorTester {
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t c = 0; c < channels(); c++) {
-          EXPECT_LE(xnn_float16_to_float(output[i * output_stride() + c]), output_max);
-          EXPECT_GE(xnn_float16_to_float(output[i * output_stride() + c]), output_min);
-          EXPECT_NEAR(xnn_float16_to_float(output[i * output_stride() + c]), output_ref[i * channels() + c], std::max(1.0e-4f, std::abs(output_ref[i * channels() + c]) * 1.0e-2f))
+          EXPECT_LE(output[i * output_stride() + c], output_max);
+          EXPECT_GE(output[i * output_stride() + c], output_min);
+          EXPECT_NEAR(output[i * output_stride() + c], output_ref[i * channels() + c], std::max(1.0e-4f, std::abs(output_ref[i * channels() + c]) * 1.0e-2f))
             << "at batch index " << i << " / " << batch_size()
             << ", channel " << c << " / " << channels();
         }

test/rsum-microkernel-tester.h
@@ -140,25 +140,25 @@ class RSumMicrokernelTester {
 
     std::vector<xnn_float16> input(batch_size() + XNN_EXTRA_BYTES / sizeof(xnn_float16));
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return xnn_float16_from_float(f32dist(rng)); });
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
 
       // Compute reference results.
       float output_ref = 0.0f;
       for (size_t i = 0; i < batch_size(); i++) {
-        output_ref += xnn_float16_to_float(input[i]);
+        output_ref += input[i];
       }
       output_ref *= scale();
 
       // Prepare parameters.
       xnn_f16_scale_params params;
-      init_params(&params, xnn_float16_from_float(scale()));
+      init_params(&params, scale());
 
       // Call optimized micro-kernel.
-      xnn_float16 output = UINT16_C(0x7E00);  /* NaN */
+      xnn_float16 output = std::nanf("");  /* NaN */
       rsum(batch_size() * sizeof(xnn_float16), input.data(), &output, &params);
 
       // Verify results.
-      EXPECT_NEAR(xnn_float16_to_float(output), output_ref, std::abs(output_ref) * 4.0e-3f)
+      EXPECT_NEAR(output, output_ref, std::abs(output_ref) * 4.0e-3f)
         << "with batch " << batch_size() << ", scale " << scale();
     }
   }
@@ -169,12 +169,12 @@ class RSumMicrokernelTester {
 
     std::vector<xnn_float16> input(batch_size() + XNN_EXTRA_BYTES / sizeof(xnn_float16));
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(input.begin(), input.end(), [&]() { return xnn_float16_from_float(f32dist(rng)); });
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
 
       // Compute reference results.
       float output_ref = 0.0f;
       for (size_t i = 0; i < batch_size(); i++) {
-        output_ref += xnn_float16_to_float(input[i]);
+        output_ref += input[i];
       }
       output_ref *= scale();
 